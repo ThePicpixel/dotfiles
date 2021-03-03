e0d4beb8ea0a24c6cefecc67e7741d74a7fef1d9
Hugo PICHON <hpichon6@gmail.com>
2021-03-03 19:45:14 +0100

    Clean the repo for my own use

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ warning(){
     echo "$(date) [!] $1" | tee -a ~/installation.log
 }
 
-alias install="sudo zypper in -y"
+alias install="sudo zypper in"
 alias upgrade="sudo zypper dup"
 
 log "Starting installation $(date)"
@@ -17,32 +17,13 @@ upgrade
 
 log "Installing wayland graphical environment"
 install\
-    rofi thunar sway  swaybg evince brightnessctl swaylock lxsession lxappearance\
-    alacritty i3status Mesa-dri
-
-# Uncomment to install X server
-#log "Installing X.org graphical environment"
-#install\
-#     i3  lxrandr xinit xorg-x11 xorg-x11-server xfce4-screenshooter xfce4-power-manager 
+    rofi thunar sway brightnessctl swaylock lxsession lxappearance\
+    alacritty i3status
+sudo zypper al 'sway*'
 
 log "Installing development tools"
 install\
      emacs git zsh podman buildah python3-virtualenv python3-devel go
-
-log "Installing reverse tools"
-install\
-     gdb glibc-32bit
-
-log "Installing radare2 dependencies"
-install\
-    gcc gcc patch meson make
-
-log "Installing gef for gdb"
-wget -q -O- https://github.com/hugsy/gef/raw/master/scripts/gef.sh | sh
-
-#log "Installing theme assets"
-#install\
-#     qt5ct elementary-icon-theme breeze5-cursors elementary-wallpapers
 
 log "Installing usefull stuff"
 install\
@@ -72,28 +53,28 @@ mkdir ~/Pictures
 mkdir ~/Vidéos
 
 cd ~/Documents
-git clone http://github.com/ThePicpixel/dotfiles
+git clone http://github.com/ThePicpixel/dotfiles --single-branch dev
 
 cp dotfiles/wallpaper.png ~/Pictures/
 
 rm -rf ~/.emacs*
 ln -s ~/Documents/dotfiles/emacs ~/.emacs.d
-mkdir ~/.config
-rm ~/.config/chromium/Default/Preferences
+
+mkdir -p ~/.config/chromium/Default
+
+
 ln -s ~/Documents/dotfiles/alacritty ~/.config
 ln -s ~/Documents/dotfiles/i3status ~/.config
 ln -s ~/Documents/dotfiles/rofi ~/.config
 ln -s ~/Documents/dotfiles/sway ~/.config
-ln -s ~/Documents/dotfiles/sakura ~/.config
-ln -s ~/Documents/dotfiles/xorg/.xinitrc ~/.xinitrc
 ln -s ~/Documents/dotfiles/git/.gitconfig ~/.gitconfig
-ln -s ~/Documents/dotfiles/albert ~/.config
 
-cp ~/Documents/dotfiles/chromium/Preferences ~/.config/chromium/Default
+cp ~/Documents/dotfiles/chromium/Preferences ~/.config/chromium/Default/Preferences
 
 log "Configure subuids and subgid to support rootless podman"
 myuser=$USER
 sudo usermod --add-subuids 10000-65536 $my_user
 sudo usermod --add-subgid 10000-65536 $my_user
+
 log "Installing oh-my-zsh"
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
